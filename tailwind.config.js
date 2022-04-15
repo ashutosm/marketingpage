63688340edf94a4bfb271389b94003bed084780a
Your Name <ashutosh@tezsure.com>
2022-04-16 00:04:07 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('http://127.0.0.1:5500/src/assets/Asset%202Fintech2%201bannerImageLight.svg')",
+        'card-image': "url('http://127.0.0.1:5500/src/assets/processorcreatetezure.svg')",
+      },
+      colors: {
+        color1light: '#FFFFFF',
+        color1dark: '#12121A',
+        color2light:'#7421EE',
+        colorBorderlight:'#76658D',
+        colorButtonBackGround:'#7421EE33',
+        headerColor:'#203152',
+        smalltextColor:'#6F6E84'
+      },
+    },
+    
+  },
+  plugins: [],
+}
